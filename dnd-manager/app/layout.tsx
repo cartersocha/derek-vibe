9a2c5d1e63bc47c25a60b4b0bc1e9ebca787bc31
cartersocha <cartersocha@gmail.com>
2025-10-21 15:36:26 -0700

    analytics

--- dnd-manager/app/layout.tsx
+++ dnd-manager/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Press_Start_2P } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 import AutoCapitalizeProvider from "@/components/providers/auto-capitalize-provider";
 
@@ -49,6 +50,7 @@ export default function RootLayout({
           {children}
         </AutoCapitalizeProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
