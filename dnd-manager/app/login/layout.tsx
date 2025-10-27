912828ee50055b0f048b1a9236ab30e433799305
cartersocha <cartersocha@gmail.com>
2025-10-27 12:07:12 -0700

    update login

--- dnd-manager/app/login/layout.tsx
+++ dnd-manager/app/login/layout.tsx
@@ -12,9 +12,5 @@ export default function LoginLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <div className="login-body">
-      {children}
-    </div>
-  );
+  return <>{children}</>;
 }
